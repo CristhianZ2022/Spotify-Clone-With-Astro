000f0d303a467eef1dc287b31d07cd34840d904e
CristhianZ2022 <cris12x12@gmail.com>
2025-11-30 21:31:27 -0500

    Separation of icons for better structure

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,34 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
+import { BlackSearchIcon, BsX } from '../icons/SearchIcons';
 
 interface SearchInputProps {
   initialQuery?: string;
 }
-
-const BlackSearchIcon = () => (
-  <svg height="36" width="36" viewBox="0 0 24 24" fill="black">
-    <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
-  </svg>
-);
-
-const BsX = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    className="icon icon-tabler icons-tabler-outline icon-tabler-x"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M18 6l-12 12" />
-    <path d="M6 6l12 12" />
-  </svg>
-);
 
 export default function SearchButton({ initialQuery = '' }: SearchInputProps) {
   const [value, setValue] = useState(initialQuery);
@@ -46,9 +22,8 @@ export default function SearchButton({ initialQuery = '' }: SearchInputProps) {
     }
     window.history.replaceState({}, '', url);
     
-    // Dispara un evento para que Astro reaccione
     window.dispatchEvent(new CustomEvent('search-change'));
-  }, 600);
+  }, 150);
 
   const clearSearch = (e: React.MouseEvent) => {
     e.preventDefault();
