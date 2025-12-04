a824b1b5aa010d0b14676f88ab13d1d0abbcf86a
CristhianZ2022 <cris12x12@gmail.com>
2025-12-04 16:40:59 -0500

    Add double props for song or playlist function

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,36 +1,58 @@
 import { usePlayerStore } from '../store/player.Store';
 import { Play, Pause } from '../icons/PlayerIcons';
 
-function CardPlayButton({ id, size="small" }) {
+function CardPlayButton({ id, size = 'small', song }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
-    
+
   const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
 
+  const isCurrentSongRun = (song) => {
+    return (
+      currentMusic.song?.id == song.id &&
+      currentMusic.playlist?.albumId == song.albumId &&
+      isPlaying
+    );
+  };
+
   const handleClick = () => {
-    if (isPlayingPlaylist) {
+    if (isPlayingPlaylist  || isCurrentSongRun(song)) {
       setIsPlaying(false);
       return;
     }
 
-    fetch(`/api/get-info-playlist?id=${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        const { songs, playlist } = data;
-        setIsPlaying(true);
-        setCurrentMusic({ playlist, songs, song: songs[0] });
-      });
+    if (id) {
+      fetch(`/api/get-info-playlist?id=${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          const { songs, playlist } = data;
+          setCurrentMusic({ playlist, songs, song: songs[0] });
+          setIsPlaying(true);
+        });
+    } else {
+      fetch(`/api/get-info-playlist?id=${song.albumId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          const { songs, playlist } = data;
+          setCurrentMusic({ playlist, songs, song: song });
+          setIsPlaying(true);
+        });
+    }
   };
 
-  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5';
 
   return (
     <button
-      aria-label='Reproducir'
+      aria-label="Reproducir"
       className="card-play-button rounded-full bg-red-900 p-4 hover:scale-100 transition hover:bg-red-700"
       onClick={handleClick}
     >
-      {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
+      {isPlayingPlaylist ? (
+        <Pause className={iconClassName} />
+      ) : (
+        <Play className={iconClassName} />
+      )}
     </button>
   );
 }
